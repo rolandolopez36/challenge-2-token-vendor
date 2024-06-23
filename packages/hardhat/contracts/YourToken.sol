c63b279c2567410ceca1456ba7642ed47a84f865
rolandolopez36 <tuemail@example.com>
2024-06-23 15:34:29 +0200

    feat: Update ERC20 implementation in YourToken.sol

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract YourToken is ERC20 {
-    constructor() ERC20("Scaffold ETH Token", "SET") {
-        _mint(msg.sender, 1000 * 10 ** 18);
-    }
+  constructor() ERC20("Gold", "GLD") {
+    //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
+  }
 }
